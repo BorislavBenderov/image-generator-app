0dfb9056d460b157d48f7eea8e86c7da839c3be5
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-22 21:31:33 +0200

    Get posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import {} from ".";
 import { useState } from "react";
 import { FormField, Loader } from "../components";
+import { useEffect } from "react";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
@@ -16,6 +17,31 @@ export const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -45,11 +71,11 @@ export const Home = () => {
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-                {searchText ? (
-                    <RenderCards data={[]} title="No search results found" />
-                ) : (
-                    <RenderCards data={[]} title="No posts found" />
-                )}
+              {searchText ? (
+                <RenderCards data={[]} title="No search results found" />
+              ) : (
+                <RenderCards data={allPosts} title="No posts found" />
+              )}
             </div>
           </>
         )}
