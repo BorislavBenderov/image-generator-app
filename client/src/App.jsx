27d6135800395f72328ec8883caebe0962591550
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-04 12:43:12 +0200

    Add route to NotFound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Home, CreatePost } from "./pages";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { logo } from "./assets";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
         </Routes>
